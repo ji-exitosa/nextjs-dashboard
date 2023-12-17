ca9c528463de3d87ba6785abc3668af04be5e5d6
ji-exitosa <ji.exitosa@gmail.com>
2023-12-18 00:57:07 +0900

    all

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { routeModule } from '@/node_modules/next/dist/build/templates/app-page';
+
+export default function Page() {
+  return <p>인보이스 Page</p>;
+}
+
+// app / dashboard / invoices / page.tsx;
+// 위 파일명으로 하면 되는데,
+// 왜 route.js나 route.tsx로는 안되지
---
+++ app/page.tsx
@@ -0,0 +1,62 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* 이 영역 왜 주석처리 되어있던거지...? 어차피 저 컴포넌트 안에 lusitana import 되어있어서 여기서 lusitana import 안해도 문제 없어보이는데 */}
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          {/* 전역 css 파일에서 @tailwind 이용하는 방법 */}
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          {/* home.modules.css 에서 @tailwind 이용하는 방법 --> 불가능.. 왜인지는 조금 더 보기 */}
+          {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
+          {/* home.modules.css 추가해서 class명으로 스타일 불러오는 방법 */}
+          <div className={styles.shape} />
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            Welcome to Acme. This is the example for the Next.js Learn Course,
+            brought to you by Vercel.
+          </p>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , brought to you by Vercel.
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
